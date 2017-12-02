3cc03d0c31481953d6e9888f6831687a2c81c9ba
K4YT3X <k4t@protonmail.com>
2017-12-02 13:27:23 -0500

    Packed functions into classes for importing

--- kpm.py
+++ kpm.py
@@ -11,7 +11,7 @@
 Name: K4YT3X (APT) Package Manager
 Author: K4T
 Date: 3/24/17
-Last Modified: AUG 30, 2017
+Last Modified: DEC 2, 2017
 
 Description: KPM is an automatic apt management system
     simply use command "kpm" to automatically update apt cache,
@@ -67,7 +67,7 @@ except ImportError:
         else:
             print('\033[31m\033[1mInvalid Input!\033[0m')
 
-VERSION = '1.4.7'
+VERSION = '1.5.1'
 
 ImportList = []
 
@@ -122,155 +122,12 @@ def process_arguments():
     args = parser.parse_args()
 
 
-def upgrade_safe():
-    output = subprocess.Popen(["apt-get", "upgrade", "-s"], stdout=subprocess.PIPE).communicate()[0]
-    output = output.decode().split('\n')
-    for line in output:
-        parsedLine = line.replace('and', ',').replace(' ', '').split(',')
-        try:
-            if parsedLine[2] == '0toremove':
-                return True
-        except IndexError:
-            pass
-    return False
+class kpm:
 
-
-def dist_upgrade_safe():
-    output = subprocess.Popen(["apt-get", "dist-upgrade", "-s"], stdout=subprocess.PIPE).communicate()[0]
-    output = output.decode().split('\n')
-    for line in output:
-        parsedLine = line.replace('and', ',').replace(' ', '').split(',')
-        try:
-            if parsedLine[2] == '0toremove':
-                return True
-        except IndexError:
-            pass
-    return False
-
-
-def update():
-    global ImportList
-    output = ''
-    process = subprocess.Popen(['apt-get', 'update'], stdout=subprocess.PIPE)
-    for c in iter(lambda: process.stdout.read(1), ''):
-        if not c:
-            break
-        sys.stdout.write(c.decode())
-        output += c.decode()
-    for line in output.split('\n'):
-        if 'NO_PUBKEY' in line:
-            ImportList.append(line.split(' ')[-1].replace('\n', ''))
-
-
-def dist_upgrade():
-    os.system('apt-get dist-upgrade -y')
-
-
-def upgrade():
-    os.system('apt-get upgrade -y')
-
-
-def manual_dist_upgrade():
-    os.system('apt-get dist-upgrade')
-
-
-def manual_upgrade():
-    os.system('apt-get upgrade')
-
-
-def list_upgrades():
-    os.system('apt list --upgradable')
-
-
-def showHold():
-    output = subprocess.Popen(["apt-mark", "showhold"], stdout=subprocess.PIPE).communicate()[0]
-    output = output.decode().split('\n')
-    avalon.warning('Following Packages marked hold and will not be upgraded:\n')
-    for line in output:
-        print(avalon.FG.R + line)
-    print()
-
-
-def noUpgrades():
-    output = subprocess.Popen(["apt-get", "dist-upgrade", "-s"], stdout=subprocess.PIPE).communicate()[0]
-    output = output.decode().split('\n')
-    for line in output:
-        parsedLine = line.replace('and', ',').replace(' ', '').split(',')
-        if parsedLine[0] == '0upgraded' and parsedLine[1] == '0newlyinstalled':
-            if parsedLine[3] != '0notupgraded.':
-                showHold()
-            return True
-    return False
-
-
-def internet_connected():
-    """
-    This fucntion detects if the internet is available
-    Returns a Boolean value
-    """
-    print(avalon.FG.Y + '[+] INFO: ' + avalon.FG.W + 'Checking Internet to Google ------- ' + avalon.FM.RST, end='')
-    try:
-        socket.create_connection(('www.google.ca', 443), 1)  # Test connection by connecting to google
-        print(avalon.FG.G + avalon.FM.BD + 'OK!' + avalon.FM.RST)
-        return True
-    except socket.error:
-        print(avalon.FG.R + 'Google No Respond' + avalon.FM.RST)
-        try:
-            print(avalon.FG.Y + '[+] INFO: ' + avalon.FG.W + 'Checking Internet to DNS ---------- ' + avalon.FM.RST, end='')
-            socket.create_connection(('8.8.8.8', 53), 1)  # Test connection by connecting to google
-            print(avalon.FG.G + avalon.FM.BD + 'OK!' + avalon.FM.RST)
-            return True
-        except socket.error:
-            print(avalon.FG.R + 'Server Timed Out!' + avalon.FM.RST)
-            return False
-
-
-# --------------------------------Procedural Code--------------------------------
-
-try:
-    icon()
-    process_arguments()
-    if os.getuid() != 0:
-        avalon.error('This program must be run as root!')
-        exit(0)
-    if internet_connected():
+    def __init__(self):
         pass
-    else:
-        avalon.error('Internet not connected! Aborting...')
-        exit(0)
 
-    check_version()
-
-    avalon.info('Starting KPM Sequence')
-    if args.installkpm:
-        os.system('cp ' + os.path.abspath(__file__) + ' /usr/bin/kpm')  # os.rename throws an error when /tmp is in a separate partition
-        os.system('chown root: /usr/bin/kpm')
-        os.system('chmod 755 /usr/bin/kpm')
-        avalon.info('KPM Successfully installed!')
-        avalon.info('Now you can type "kpm" to start KPM')
-        exit(0)
-    elif args.install:
-        packages = args.install.split(',')
-        empty_packages = False
-        for pkg in packages:
-            if len(pkg) == 0:
-                empty_packages = True
-        if empty_packages:
-            avalon.error('Please Provide Valid Package Names!')
-            exit(0)
-        for pkg in packages:
-            avalon.info('Installing Package: ' + pkg)
-            os.system('apt-get install ' + pkg)
-    elif args.search:
-        avalon.info('Searching in PT Cache...')
-        os.system(('apt-cache search ' + args.search + ' | grep --color=auto -E "^|' + args.search + '"'))
-    elif args.version:
-        avalon.info('Getting Versions for Package ' + args.version)
-        os.system('apt-cache madison ' + args.version)
-    elif args.autoremove:
-        avalon.info('Auto Removing Extra Packages From System')
-        os.system('apt-get autoremove')
-    else:
+    def upgrade_all(self):
         avalon.info('Starting Automatic Upgrade Procedure...')
         avalon.info('Updating APT Cache...')
         with open('/etc/apt/sources.list', 'r') as aptlist:
@@ -285,7 +142,7 @@ try:
                         aptlist.close()
                         exit(0)
             aptlist.close()
-        update()
+        self.update()
         print(avalon.FG.G + '[+] INFO: Updated!' + avalon.FG.W)
         if len(ImportList) != 0:
             if avalon.ask('Detected unimported keys! Import?', True):
@@ -305,27 +162,168 @@ try:
                 else:
                     for key in ImportList:
                         os.system('apt-key adv --keyserver keyserver.ubuntu.com --recv ' + key)
-            update()  # Second update after keys are imported
-        if noUpgrades():
+            self.update()  # Second update after keys are imported
+        if self.noUpgrades():
             avalon.info('All Packages are up to date')
             avalon.info('No upgrades available')
         else:
             avalon.info('Checking if Upgrade is Safe...')
-            if upgrade_safe():
+            if self.upgrade_safe():
                 avalon.info('Upgrade Safe! Starting Upgrade...')
-                upgrade()
+                self.upgrade()
             else:
                 avalon.warning('Upgrade Not Safe! Using Manual Upgrade...')
-                manual_upgrade()
+                self.manual_upgrade()
 
             avalon.info('Checking if Dist-Upgrade is Safe...')
-            if dist_upgrade_safe():
+            if self.dist_upgrade_safe():
                 avalon.info('Dist-Upgrade Safe! Starting Upgrade...')
-                dist_upgrade()
+                self.dist_upgrade()
             else:
                 avalon.warning('Dist-Upgrade not Safe! Using Manual Upgrade...')
-                manual_dist_upgrade()
+                self.manual_dist_upgrade()
         avalon.info('Upgrade Procedure Completed!')
-    avalon.info('KPM Sequence Completed!')
-except KeyboardInterrupt:
-    avalon.warning('Aborting...')
+
+    def upgrade_safe(self):
+        output = subprocess.Popen(["apt-get", "upgrade", "-s"], stdout=subprocess.PIPE).communicate()[0]
+        output = output.decode().split('\n')
+        for line in output:
+            parsedLine = line.replace('and', ',').replace(' ', '').split(',')
+            try:
+                if parsedLine[2] == '0toremove':
+                    return True
+            except IndexError:
+                pass
+        return False
+
+    def dist_upgrade_safe(self):
+        output = subprocess.Popen(["apt-get", "dist-upgrade", "-s"], stdout=subprocess.PIPE).communicate()[0]
+        output = output.decode().split('\n')
+        for line in output:
+            parsedLine = line.replace('and', ',').replace(' ', '').split(',')
+            try:
+                if parsedLine[2] == '0toremove':
+                    return True
+            except IndexError:
+                pass
+        return False
+
+    def update(self):
+        global ImportList
+        output = ''
+        process = subprocess.Popen(['apt-get', 'update'], stdout=subprocess.PIPE)
+        for c in iter(lambda: process.stdout.read(1), ''):
+            if not c:
+                break
+            sys.stdout.write(c.decode())
+            output += c.decode()
+        for line in output.split('\n'):
+            if 'NO_PUBKEY' in line:
+                ImportList.append(line.split(' ')[-1].replace('\n', ''))
+
+    def dist_upgrade(self):
+        return os.system('apt-get dist-upgrade -y')
+
+    def upgrade(self):
+        return os.system('apt-get upgrade -y')
+
+    def manual_dist_upgrade(self):
+        return os.system('apt-get dist-upgrade')
+
+    def manual_upgrade(self):
+        return os.system('apt-get upgrade')
+
+    def list_upgrades(self):
+        return os.system('apt list --upgradable')
+
+    def showHold(self):
+        output = subprocess.Popen(["apt-mark", "showhold"], stdout=subprocess.PIPE).communicate()[0]
+        output = output.decode().split('\n')
+        avalon.warning('Following Packages marked hold and will not be upgraded:\n')
+        for line in output:
+            print(avalon.FG.R + line)
+        print()
+
+    def noUpgrades(self):
+        output = subprocess.Popen(["apt-get", "dist-upgrade", "-s"], stdout=subprocess.PIPE).communicate()[0]
+        output = output.decode().split('\n')
+        for line in output:
+            parsedLine = line.replace('and', ',').replace(' ', '').split(',')
+            if parsedLine[0] == '0upgraded' and parsedLine[1] == '0newlyinstalled':
+                if parsedLine[3] != '0notupgraded.':
+                    self.showHold()
+                return True
+        return False
+
+    def internet_connected(self):
+        """
+        This fucntion detects if the internet is available
+        Returns a Boolean value
+        """
+        print(avalon.FG.Y + '[+] INFO: ' + avalon.FG.W + 'Checking Internet to Google ------- ' + avalon.FM.RST, end='')
+        try:
+            socket.create_connection(('www.google.ca', 443), 1)  # Test connection by connecting to google
+            print(avalon.FG.G + avalon.FM.BD + 'OK!' + avalon.FM.RST)
+            return True
+        except socket.error:
+            print(avalon.FG.R + 'Google No Respond' + avalon.FM.RST)
+            try:
+                print(avalon.FG.Y + '[+] INFO: ' + avalon.FG.W + 'Checking Internet to DNS ---------- ' + avalon.FM.RST, end='')
+                socket.create_connection(('8.8.8.8', 53), 1)  # Test connection by connecting to google
+                print(avalon.FG.G + avalon.FM.BD + 'OK!' + avalon.FM.RST)
+                return True
+            except socket.error:
+                print(avalon.FG.R + 'Server Timed Out!' + avalon.FM.RST)
+                return False
+
+
+# --------------------------------Procedural Code--------------------------------
+
+if __name__ == '__main__':
+    try:
+        icon()
+        process_arguments()
+        kobj = kpm()
+        if os.getuid() != 0:
+            avalon.error('This program must be run as root!')
+            exit(0)
+        if not kobj.internet_connected():
+            avalon.error('Internet not connected! Aborting...')
+            exit(0)
+
+        check_version()
+
+        avalon.info('Starting KPM Sequence')
+        if args.installkpm:
+            os.system('cp ' + os.path.abspath(__file__) + ' /usr/bin/kpm')  # os.rename throws an error when /tmp is in a separate partition
+            os.system('chown root: /usr/bin/kpm')
+            os.system('chmod 755 /usr/bin/kpm')
+            avalon.info('KPM Successfully installed!')
+            avalon.info('Now you can type "kpm" to start KPM')
+            exit(0)
+        elif args.install:
+            packages = args.install.split(',')
+            empty_packages = False
+            for pkg in packages:
+                if len(pkg) == 0:
+                    empty_packages = True
+            if empty_packages:
+                avalon.error('Please Provide Valid Package Names!')
+                exit(0)
+            for pkg in packages:
+                avalon.info('Installing Package: ' + pkg)
+                os.system('apt-get install ' + pkg)
+        elif args.search:
+            avalon.info('Searching in PT Cache...')
+            os.system(('apt-cache search ' + args.search + ' | grep --color=auto -E "^|' + args.search + '"'))
+        elif args.version:
+            avalon.info('Getting Versions for Package ' + args.version)
+            os.system('apt-cache madison ' + args.version)
+        elif args.autoremove:
+            avalon.info('Auto Removing Extra Packages From System')
+            os.system('apt-get autoremove')
+        else:
+            kobj.upgrade_all()
+        avalon.info('KPM Sequence Completed!')
+    except KeyboardInterrupt:
+        avalon.warning('Aborting...')
